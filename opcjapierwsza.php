77808e4d7a199688fbc3d14830040f7cf226a66f
ocvxlast <65806818+DavidK4enig@users.noreply.github.com>
2023-11-20 12:52:44 +0100

    Add files via upload

--- opcjapierwsza.php
+++ opcjapierwsza.php
@@ -12,12 +12,12 @@
         <form method="POST" action="">
         kwota udzielonego kredytu: <input type="text" name="A"><br>
         oprocentowanie w skali roku: <input type="text" name="b"><br>
-        liczba miesiecy: <input type="text" name="m"><br>
-        liczba rat: <input type="text" name="n"><br><br>
+        ilosc rat w roku: <input type="text" name="m"><br>
+        calosciowa liczba rat: <input type="text" name="n"><br><br>
         <input type="submit" value="Oblicz pierwszym wzorem" name='submita'><br><br>
         <?php
     // oprocentowanie w skali roku to oprocentowanie nominalne
-    // wzor na wysokosc raty: R=A*(1+(b/m)^n)*(1+(b/m)-1)/[(1+(b/m)^n)-1] | zrodlo: wzoropcjapierwsza.png
+    // wynikjeden na wysokosc raty: R=A*(1+(b/m)^n)*(1+(b/m)-1)/[(1+(b/m)^n)-1] | zrodlo: wzoropcjapierwsza.png
     // jeden to wysokosc_raty         | trzeba okreslic
     // A to kwota_udzielonego_kredytu | okreslone
     // b to oprocentowanie_nominalne  | okreslone
@@ -28,9 +28,12 @@
         $b = $_POST['b'];
         $m = $_POST['m'];
         $n = $_POST['n'];
-        $drugien = (float)$n*0.01;
-        $jeden = (float)$A*(1+((float)$b/(float)$m)*(float)$drugien*(float)$drugien)*(1+((float)$b/(float)$m)-1)/((1+((float)$b/(float)$m)*(float)$drugien*(float)$drugien)-1);
-        echo $jeden;
+        $drugieb = (float)$b*0.01;
+        $jeden = (float)$A*(1+((float)$drugieb/(float)$m)**(float)$n); // A*(1+(b/m)^n) jako czesc1
+        $dwa = (1+((float)$drugieb/(float)$m)-1); // (1+(b/m)-1) jako czesc2
+        $trzy = [(1+((float)$b/(float)$m)**(float)$n)-1]; // [(1+(b/m)^n)-1] jako czesc3
+        $wynikjeden = ((float)$jeden*(float)$dwa)/(float)$trzy; // R=[(czesc1)*(czesc2)]/(czesc3)
+        echo $wynikjeden;
        }
         ?>
         zlotych
@@ -41,20 +44,31 @@
     </div>
     <div class="wysokosc_raty_dwa">
         <br><br>
+        opcja druga:<br><br>
+        <form method="POST" action="">
+        kwota udzielonego kredytu: <input type="text" name="o"><br>
+        oprocentowanie w skali roku: <input type="text" name="p"><br>
+        ilosc rat w roku: <input type="text" name="r"><br>
+        calosciowa liczba rat: <input type="text" name="s"><br><br>
     <input type="submit" value="Oblicz drugim wzorem" name='submitb'><br><br>
         <?php
     // oprocentowanie w skali roku to oprocentowanie nominalne
-    // wzor na wysokosc raty:  | zrodlo: wzór opracowany przez uczniów
-    // dwa to wysokosc_raty           | trzeba okreslic
-    // b to kwota_udzielonego_kredytu | okreslone
-    // c to oprocentowanie_nominalne  | okreslone
-    // d to ilosc_rat                 | okreslone
+    // wzor na wysokosc raty:  | zrodlo: wzoropcjadruga.png
+    // wynikdwa to wysokosc_raty      | trzeba okreslic
+    // o to kwota_udzielonego_kredytu | okreslone
+    // p to oprocentowanie_nominalne  | okreslone
+    // r to ilosc_rat_w_roku          | okreslone
+    // s to calosciowa_liczba_rat     | okreslone
        if(isset($_POST['submitb'])) {
-        $b = $_POST['b'];
-        $c = $_POST['m'];
-        $d = $_POST['n'];
-        $dwa = (float)$b*(1+((float)$c/100))*(float)$d;
-        echo $dwa;
+        $o = $_POST['o'];
+        $p = $_POST['p'];
+        $drugiep = (float)$p*0.01;
+        $r = $_POST['r'];
+        $s = $_POST['s'];
+        $gora = ((float)$o*(float)$drugiep);
+        $dol = (float)$r*(1-(((float)$r)/((float)$r+(float)$drugiep))**(float)$s);
+        $wynikdwa = (float)$gora/(float)$dol;
+        echo $wynikdwa;
        }
         ?>
         zlotych
@@ -62,12 +76,7 @@
     </div>
     <div class="wzordwa">
         <br>
-        <h3>a = b*(1+(c/100))*d<br><br>
-        a to wysokosc jednej raty<br>
-        b to kwota udzielonego kredytu<br>  
-        c to oprocentowanie nominalne (roczne)<br>
-        d to ilość rat<br><br></h3>
-        wzór opracowany przez uczniów
+    <img src="wzoropcjadruga.png" alt="drugi wzor na wysokosc raty">
     </div>
 </body>
 </html>
