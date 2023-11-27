5911ffd2829e123bb2b1a803a4ccdf3acc2f87a4
ocvxlast <65806818+DavidK4enig@users.noreply.github.com>
2023-11-27 12:38:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
         do obliczania wysokości poszczególnych rat kredytu<br>
         przy założonym poziomie oprocentowania nominalnego,<br>
         okresie na jaki kredyt został zaciągnięty,<br>
-        oraz sposobie spłat rat kredytu:<br>
+        oraz sposobie spłat rat kredytu:<br><br>
         <form action="opcjapierwsza.php">
             <input type="submit" value="Kliknij tutaj" />
         </form>
@@ -19,7 +19,7 @@
     <div class="opcjadruga">
         Kalkulator lokat służący do obliczania przyszłej wartości inwestycji w lokaty<br>
         bankowe, przy założonym oprocentowaniu nominalnym, kapitalizacji oraz okresie na jaki <br>
-        została założona lokata:<br>
+        została założona lokata:<br><br>
         <form action="opcjadruga.php">
             <input type="submit" value="Kliknij tutaj" />
         </form>
